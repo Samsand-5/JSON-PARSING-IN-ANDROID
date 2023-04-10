c73da429ec8c92f5870cb47cb2c3a5028d1d9c16
Soumyodip Sutradhar <74486329+Samsand-5@users.noreply.github.com>
2023-04-10 22:26:50 +0530

    Create ModelClass.kt

---
+++ RETROFIT LIBRARY/ModelClass.kt
@@ -0,0 +1,12 @@
+package com.gtappdevelopers.kotlingfgproject
+ 
+data class CourseDataModal(
+    // on below line creating variables for our modal class
+    // make sure that variable name should be same to
+    // that of key which is used in json response. 
+    var courseName: String,
+    var courseimg: String,
+    var courseDesc: String,
+    var Prerequisites: String,
+    var courseLink: String
+)
